9d820c16bac6789d6c1629e9f90624d5f7505f9f
zhanfeng.hzf <zhanfeng.hzf@alibaba-inc.com>
2020-05-21 00:12:38 +0800

    ober@4.0.9

--- src/LoseProxy.ts
+++ src/LoseProxy.ts
@@ -34,7 +34,7 @@ export function createReactableMember<T extends object>(
         : shadow[member];
       if (isArray(value)) {
         return wrapReactableArray(value, handler, () => {
-          handler.get(this, member, this);
+          handler.get(shadow, member, shadow);
         });
       } else {
         return value;
