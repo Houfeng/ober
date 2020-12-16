a170a8a23c96aebacdb01aa9ea6a43b6ceea1848
zhanfeng.hzf <zhanfeng.hzf@alibaba-inc.com>
2020-12-16 19:59:57 +0800

    + ober@6.4.0

--- src/ObserveConfig.ts
+++ src/ObserveConfig.ts
@@ -10,16 +10,33 @@ export enum ObserveMode {
   auto = "auto"
 }
 
-export const ObserveConfig: {
+export interface ObserveConfigDefinition {
   mode: ObserveMode;
   strict: boolean;
   maxDependencies: number;
   maxHandlers: number;
   logPrefix: string;
-} = {
+}
+
+export const DEFAULT_LOG_PREFIX = "OBER";
+
+export const ObserveEnvConfig: Partial<ObserveConfigDefinition> = (() => {
+  if (typeof process === "undefined") return {};
+  const configText: string = process.env && process.env.OBER_CONFIG;
+  if (!configText) return {};
+  try {
+    return JSON.parse(configText) || {};
+  } catch {
+    const prefix = DEFAULT_LOG_PREFIX;
+    throw new Error(`${prefix}: "${prefix}_CONFIG" is incorrect`);
+  }
+})();
+
+export const ObserveConfig: ObserveConfigDefinition = {
   mode: ObserveMode.auto,
   strict: false,
   maxDependencies: 1000,
   maxHandlers: 100,
-  logPrefix: "OBER"
+  logPrefix: DEFAULT_LOG_PREFIX,
+  ...ObserveEnvConfig
 };
